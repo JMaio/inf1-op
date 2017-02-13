e120c87d1db7107c9885c2c388d95918b6a67d5d
JMaio <j.f.maio98@gmail.com>
2017-02-13 18:44:55 +0000

    start week 6

---
+++ src/week6/CreditCard.java
@@ -0,0 +1,18 @@
+package week6;
+
+public class CreditCard {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public CreditCard(int expiryMonth, int expiryYear, String firstName, String lastName, String ccNumber) {
+		
+	}
+	
+	public String formatExpiryDate() {
+		
+	}
+
+}
