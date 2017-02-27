2c3b5e83bff4ae39280f2da735f050496c8b6d55
JMaio <j.f.maio98@gmail.com>
2017-02-27 22:55:53 +0000

    complete ex 3

---
+++ src/week6/CreditCardTester.java
@@ -0,0 +1,13 @@
+package week6;
+
+public class CreditCardTester {
+
+	public static void main(String[] args) {
+		
+		CreditCard cc1 = new CreditCard(10, 2010, "Bob", "Jones", "1234123412341234"); 
+		
+		System.out.println(cc1);
+		
+	}
+
+}
